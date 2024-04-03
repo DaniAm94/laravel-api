980b34e299daa75c303a22ad890b52eebf8d8186
DaniAm94 <dani.amico94@gmail.com>
2024-04-03 17:00:52 +0200

    created a slug into the technology seeder

--- database/seeders/TechnologySeeder.php
+++ database/seeders/TechnologySeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Technology;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class TechnologySeeder extends Seeder
 {
@@ -26,6 +28,7 @@ class TechnologySeeder extends Seeder
         foreach ($technologies as $technology) {
             $new_technology = new Technology();
             $new_technology->label = $technology['label'];
+            $new_technology->slug = Str::slug($new_technology->label);
             $new_technology->color = $technology['color'];
             $new_technology->save();
         }
