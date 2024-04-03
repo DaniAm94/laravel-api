6d4f41023e94593f08b7ac9bff13b663cda3d070
DaniAm94 <dani.amico94@gmail.com>
2024-04-03 17:01:31 +0200

    added an api route to get all projects related to a specific technology

---
+++ app/Http/Controllers/Api/TechnologyProjectController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Project;
+use App\Models\Technology;
+use Illuminate\Http\Request;
+
+class TechnologyProjectController extends Controller
+{
+    public function __invoke(string $slug)
+    {
+        $technology = Technology::whereSlug($slug)->first();
+        if (!$technology) return response(null, 404);
+        $projects = Project::whereHas('technologies', function ($query) use ($technology) {
+            $query->where('technologies.id', $technology->id)->whereIsCompleted(true);
+        })->get();
+        return response()->json(['projects' => $projects, 'label' => $technology->label]);
+    }
+}
