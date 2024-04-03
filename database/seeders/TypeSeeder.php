2324430b68a198a8a010c5c57d982d52149e53c0
DaniAm94 <dani.amico94@gmail.com>
2024-04-03 15:47:44 +0200

    added slug column to types table

--- database/seeders/TypeSeeder.php
+++ database/seeders/TypeSeeder.php
@@ -6,6 +6,8 @@ use App\Models\Type;
 use Faker\Generator as Faker;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
 
 class TypeSeeder extends Seeder
 {
@@ -18,6 +20,7 @@ class TypeSeeder extends Seeder
         foreach ($labels as $label) {
             $type = new Type();
             $type->label = $label;
+            $type->slug = Str::slug($type->label);
             $type->color = $faker->hexColor();
             $type->save();
         }
