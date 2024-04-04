303a6a843b1f8cb0cf9479a14edcb4a4ce09c0b3
DaniAm94 <dani.amico94@gmail.com>
2024-04-04 23:22:26 +0200

    update message action into the ContactController

--- app/Http/Controllers/Api/ContactController.php
+++ app/Http/Controllers/Api/ContactController.php
@@ -3,11 +3,17 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Mail\ContactMessageMail;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
 
 class ContactController extends Controller
 {
-    public function message()
+    public function message(Request $request)
     {
+        $data = $request->all();
+        $mail = new ContactMessageMail($data['sender'], $data['subject'], $data['content']);
+        Mail::to(env('MAIL_TO_ADDRESS'))->send($mail);
+        return response(null, 200);
     }
 }
